86c2804c9de0574461fc25918d058e330c600272
JayaSharma271308 <jayasharma72230@gmail.com>
2026-07-22 13:18:13 +0530

    Added Python internship tasks

---
+++ Task21-07-2026/task1.py
@@ -0,0 +1,16 @@
+'''Task1:Basic pythn program
+Create a program that takes the following information from the user:
+Name
+Age
+Email
+City
+Display all information in a properly formatted message.'''
+
+name=input("Enter Your Name:")
+age=int(input("Enter your age:"))
+email=input("Enter your email here:")
+city=input("Enter your city:")
+print(f"Your Name is {name}")
+print(f"Your Age is {age}")
+print(f"Your Email is {email}")
+print(f"Your City is {city}")
